ad7d1999697449cf70b10f7eded536efe50b7543
Ruslan <ruslan@example.com>
2025-04-30 20:48:34 +0300

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <Arduino.h>
 #include <stdio.h>
 
-#define MAX_INPUT_LENGTH 64 // Maximum length of the input buffer
+#define MAX_INPUT_LENGTH 60 // Maximum length of the input buffer
+#define MAX_ARGS_COUNT 5 // Maximum number of arguments
+#define MAX_ARG_LENGTH 12 // Maximum length of each argument
 
 int readStringFromSerial(char *buffer, int maxLength);
 
@@ -11,34 +13,52 @@ void setup()
 {
     Serial.begin(9600);
 
-    // Print a welcome message
     Serial.println("Arduino Serial Echo");
     Serial.println("Type something to see it echoed back!");
 }
 
-void loop()
-{
-    // Serial.println("loop() ");
+void loop() {
+    char argsArray[MAX_ARGS_COUNT][MAX_ARG_LENGTH]; // Array to hold the arguments
     int length = readStringFromSerial(inputBuffer, MAX_INPUT_LENGTH);
+    char *token;
 
-    if (length > 0)
-    {
+    if (length > 0) {
         // Print the received string
         Serial.print("You typed [");
         Serial.print(inputBuffer);
         Serial.print("]\n");
+
+        token = strtok(inputBuffer, " "); // Tokenize the input string
+        for (int i = 0; i < MAX_ARGS_COUNT; i++) {
+            if (token == NULL) {
+                argsArray[i][0] = '\0';
+                break;
+            }
+            strcpy(argsArray[i], token);
+            token = strtok(NULL, " ");
+        }
+
+        // Print the arguments
+        Serial.print("Arguments:\n");
+        for (int i = 0; i < MAX_ARGS_COUNT; i++) {
+            if (argsArray[i][0] == '\0') {
+                break;
+            }
+            Serial.print("arg[");
+            Serial.print(i);
+            Serial.print("]: ");
+            Serial.println(argsArray[i]);
+        }
     }
 }
 
 // Function that reads a string from serial until newline
 // Returns: number of characters read (0 if nothing or only newline was read)
-int readStringFromSerial(char *buffer, int maxLength)
-{
+int readStringFromSerial(char *buffer, int maxLength) {
     int index = 0;
 
     // Wait for data to be available
-    while (1)
-    {
+    while (1) {
         if (Serial.available() <= 0) {
             continue; // No data available, keep waiting
         }
@@ -46,10 +66,8 @@ int readStringFromSerial(char *buffer, int maxLength)
         char incomingChar = Serial.read();
 
         // If the incoming char is a newline, terminate the string
-        if (incomingChar == '\n' || incomingChar == '\r')
-        {
-            if (index > 0)
-            { // Only process if buffer contains data
+        if (incomingChar == '\n' || incomingChar == '\r') {
+            if (index > 0) { // Only process if buffer contains data
                 Serial.write('\n'); // Echo the newline character
                 // Add null terminator to make it a proper C string
                 buffer[index] = '\0';
@@ -58,8 +76,7 @@ int readStringFromSerial(char *buffer, int maxLength)
             }
         }
         // Otherwise, add the incoming character to the buffer if there's room
-        else if (index < maxLength - 1)
-        {
+        else if (index < maxLength - 1) {
             Serial.write(incomingChar); // Echo the character back immediately
             // Leave room for null terminator
             buffer[index] = incomingChar;
